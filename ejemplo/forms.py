e687b045626e2644c79125660e55bdee642fbfae
juuliadominguez <spandonarijulia@gmail.com>
2022-12-12 17:16:02 -0300

    agrego ModelForm para Familiar

--- ejemplo/forms.py
+++ ejemplo/forms.py
@@ -1,4 +1,13 @@
 from django import forms
+from ejemplo.models import Familiar
+
 
 class Buscar(forms.Form):
-    nombre = forms.CharField(max_length=100)
+    nombre = forms.CharField(max_length=100,
+                             widget= forms.TextInput(attrs={'placeholder': 'Busque algo...'}))
+
+class FamiliarForm(forms.ModelForm):
+    class Meta:
+        model = Familiar
+        fields = ['nombre', 'direccion', 'numero_pasaporte', 'fecha_de_nacimiento']
+
